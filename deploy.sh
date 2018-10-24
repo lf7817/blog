39f118ff9515d95052a07502ddfb63fe21c40607
lf7817 <535536456@qq.com>
2018-10-24 22:31:33 +0800

    修改配置1

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,14 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
+git config user.name "${U_NAME}"
+git config user.email "${U_EMAIL}"
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lf7817/blog.git master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:${P_BRANCH}
 
 cd -
